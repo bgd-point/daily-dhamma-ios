8d788db39c4400d181354ffa1ff688fbc406d943
Martien Dermawan Tanama <martiendt@gmail.com>
2018-05-21 01:22:36 +0700

    add home (daily) layout

--- Daily Dhamma/ViewController.swift
+++ Daily Dhamma/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var questionTextView: UITextView!
+    
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBOutlet var answerTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World");
+        
+        var ref: DatabaseReference!
+        
+        ref = Database.database().reference()
+        
+        print(ref)
+        
+        ref.child("dhamma-today").observe(DataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? [String : AnyObject] ?? [:]
+            print(value)
+            
+            let questionHtml = value["question"] as? String ?? ""
+            let answerHtml = value["answer"] as? String ?? ""
+            
+            let questionHtmlData = NSString(string: questionHtml).data(using: String.Encoding.unicode.rawValue)
+            let answerHtmlData = NSString(string: answerHtml).data(using: String.Encoding.unicode.rawValue)
+            
+            let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html]
+            
+            let questionAttributedString = try! NSAttributedString(data: questionHtmlData!, options: options, documentAttributes: nil)
+            let answerAttributedString = try! NSAttributedString(data: answerHtmlData!, options: options, documentAttributes: nil)
+            
+            // self.questionTextView.isScrollEnabled = false
+            self.questionTextView.isEditable = false
+            self.questionTextView.attributedText = questionAttributedString
+            self.questionTextView.sizeToFit()
+            
+            self.answerTextView.isScrollEnabled = false
+            self.answerTextView.isEditable = false
+            self.answerTextView.attributedText = answerAttributedString
+            self.answerTextView.sizeToFit()
+            
+            
+        })
+
+
     }
 
     override func didReceiveMemoryWarning() {
