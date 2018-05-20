8b59a5b7ae739aba732c2c98da8ca445d96a0584
Martien Dermawan Tanama <martiendt@gmail.com>
2018-05-20 10:28:02 +0700

    Initial Commit

---
+++ Daily Dhamma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily Dhamma
+//
+//  Created by Martien DT on 5/20/18.
+//  Copyright © 2018 Martien Dermawan Tanama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
